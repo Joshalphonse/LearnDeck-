ff3578af9a9ee702f785c76695db622c32349aeb
Rob Arcand <robbyarcand@aol.com>
2022-06-07 17:50:34 -0400

    changed h1 text and added red background

--- src/components/heroimage.tsx
+++ src/components/heroimage.tsx
@@ -5,7 +5,7 @@ import "./heroimage.css";
 const Hero: React.FC = () => {
   return (
     <div>
-      <h1 className="citrus-labs-logo">Hi Rob</h1>
+      <h1 className="citrus-labs-logo">Hi Josh</h1>
       <img src={Heroimage} className="hero" alt="logo" />
     </div>
   );
